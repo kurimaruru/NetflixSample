547024d1bbef573b7b4dad6b453d0c0b1cb9da04
kurimaru <kurikuribaseball530@gmail.com>
2022-03-05 14:27:26 +0900

    second commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {Row} from "./components/Row";
+import {Nav} from "./components/Nav";
+import {Banner} from "./components/Banner";
+import { requests } from "./request";
+
+function App(){
+  return (
+    <div className="App">
+      <Nav />
+      <Banner />
+      <Row title="NETFLIX ORIGINALS"
+      fetchUrl={requests.feachNetflixOriginals}
+      isLargeRow />
+      <Row title="Top Rated" fetchUrl={requests.feactTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.feactActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.feactComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.feactHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.feactRomanceMovies} />
+      <Row title="DOcumentaries" fetchUrl={requests.feactDocumentMovies} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/request.js
@@ -0,0 +1,12 @@
+const API_KEY ="a4a4fac8fed4dcd7b4bcbc745101fe1a";
+
+export const requests = {
+    feachTrending:`/trending/all/week?api_key=${API_KEY}&language=en-us`,
+    feachNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    feactTopRated:`/discover/tv?api_key=${API_KEY}&languager=en-us`,
+    feactActionMovies:`/discover/tv?api_key=${API_KEY}&with_genres=28`,
+    feactComedyMovies:`/discover/tv?api_key=${API_KEY}&with_genres=35`,
+    feactHorrorMovies:`/discover/tv?api_key=${API_KEY}&with_genres=27`,
+    feactRomanceMovies:`/discover/tv?api_key=${API_KEY}&with_genres=10749`,
+    feactDocumentMovies:`/discover/tv?api_key=${API_KEY}&with_genres=99`,
+}
